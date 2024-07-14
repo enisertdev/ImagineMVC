dfbde1f5e5e049dc4f90b08fa49b1a70aba25854
Enis Ertanrıkulu <enisertanrikulu@gmail.com>
2024-07-14 18:44:16 +0300

    implemented IGenericRepository  to GenericRepository

---
+++ Imagine/Imagine.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Imagine.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imagine.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetMany(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
