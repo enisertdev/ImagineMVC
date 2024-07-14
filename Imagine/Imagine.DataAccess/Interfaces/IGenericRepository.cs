a109ba8a20480d806e5cae13f763c8c51e8e40b6
Enis Ertanrıkulu <enisertanrikulu@gmail.com>
2024-07-14 18:42:22 +0300

    added IGenericRepository CRUD methods

---
+++ Imagine/Imagine.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imagine.DataAccess.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T Get(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetMany(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+
+    }
+}
