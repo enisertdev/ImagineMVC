90f17e1541c8bee69698de9d5aefb00b1ce36a7f
Enis Ertanrıkulu <enisertanrikulu@gmail.com>
2024-07-14 18:29:50 +0300

    added root projects

---
+++ Imagine/Imagine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Imagine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
